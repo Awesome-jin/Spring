eaffe0879190fae65dd1a7a569c66838d654a8bc
admin <admin@DESKTOP-40104>
2020-02-19 17:39:01 +0900

    Hibernate를 이용한 DB 데이터 조회, update, insert, delete

--- spring0219/src/main/java/db/hibernate/domain/Corona.java
+++ spring0219/src/main/java/db/hibernate/domain/Corona.java
@@ -6,7 +6,7 @@ import lombok.Data;
 public class Corona {
 	private int num;
 	private String nation;
-	private int infum;
+	private int infnum;
 	private int death;
 	private int cured;
 	//1. hibernate은 이름까지 똑같을 필욘 없다 (뒤에 매칭해주는데서 맞춰주기만 하면됨) 자료형을 주의히자.
